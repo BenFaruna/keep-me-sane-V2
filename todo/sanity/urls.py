e6260b2915ed88ab68edc3ee021be9eee8605850
BenFaruna <benjaminfaruna@gmail.com>
2020-11-21 04:57:48 +0100

    Completed the backend functionalities, with add and delete. Remaining the UI, using maerialize.

--- todo/sanity/urls.py
+++ todo/sanity/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'sanity'
 urlpatterns = [
     path('', views.home, name='index'),
+    path('add_todo', views.add_todo, name='add_todo'),
+    path('delete_todo/<int:todo_id>', views.delete_todo, name='delete_todo'),
 ]
--- todo/sanity/views.py
+++ todo/sanity/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils import timezone
+
+
+from .models import Todo
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    todo_items = Todo.objects.all().order_by('-date_added')
+    context = {
+        'todo_items': todo_items
+    }
+    return render(request, 'base.html', context)
+
+
+def add_todo(request):
+    content = request.POST['todo']
+    Todo.objects.create(todo=content)
+    return redirect(reverse('sanity:index'))
+
+
+def delete_todo(request, todo_id):
+    Todo.objects.get(pk=todo_id).delete()
+    return redirect(reverse('sanity:index'))
